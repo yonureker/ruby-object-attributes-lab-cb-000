9dc9041a7dec72df5551b6a6e01e99fc4ae4c245
Onur Eker <yonureker@gmail.com>
2017-12-12 18:35:07 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_new_name)
+    @name = person_new_name
+  end
+
+  def name
+    @name
+  end
+
+  def jobs=(person_new_job)
+    @jobs = person_new_job
+  end
+
+  def jobs
+    @jobs
+  end
+end
