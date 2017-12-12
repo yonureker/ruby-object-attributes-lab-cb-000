b940cc2aaeb223d5bb3d95bcc7cdbd20591d9d9e
Onur Eker <yonureker@gmail.com>
2017-12-12 18:04:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,22 @@
 class Dog
   def initialize(dog_name, breed_name)
-    @dog_name = dog_name
-    @breed_name = breed_name
+    @name = dog_name
+    @breed = breed_name
   end
 
   def name=(dog_new_name)
-    @dog_name = dog_new_name
+    @name = dog_new_name
   end
 
   def name
-    @dog_name
+    @name
   end
 
   def breed=(breed_new_name)
-    @breed_name = breed_new_name
+    @breed = breed_new_name
   end
 
   def breed
-    @breed_name
+    @breed
   end
 end
