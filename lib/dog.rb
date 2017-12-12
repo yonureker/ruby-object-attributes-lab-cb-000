588986d8b42f38a44d5a5528858b290192f0ac76
Onur Eker <yonureker@gmail.com>
2017-12-12 18:36:15 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dog_new_name)
+    @name = dog_new_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_new_name)
+    @breed = breed_new_name
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_new_name)
+    @name = person_new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(person_new_job)
+    @job = person_new_job
+  end
+
+  def job
+    @job
+  end
+end
